747dbf13131382f4b270b73f6437b6071336b120
Taturick <noturno2drick@gmail.com>
2023-08-31 11:13:43 -0300

    importando modules

---
+++ PRIMEIRO-SERVIDOR/array.js
@@ -0,0 +1,10 @@
+const array = ["Ricardo", "Rick", "Tatu","Rick"]
+
+const numbers = [1,2,3,4,5,6,818,8,84,818];
+
+module.exports = array;
+
+module.exports = {
+    arrayNumeros: numbers,
+    arrayNomes: array
+};
--- PRIMEIRO-SERVIDOR/index.js
+++ PRIMEIRO-SERVIDOR/index.js
@@ -1,8 +1,19 @@
-const lodash = require('lodash');
+const { uniq }= require('lodash');
+const {arrayNomes, arrayNumeros} = require("lodash")
 
-const array = [1,2,3,4,4,6,7,1]
+const array = require('./array');
 
-const arrayUnico = lodash.uniq(array);
+const arrays = [1,2,3,4,4,6,7,1];
 
-console.log(arrayUnico)
+console.log(arrays)
+const arraysUnic = uniq(arrays)
+console.log(arraysUnic)
+
+
+console.log(array.arrayNumeros, array.arrayNomes)
+const arrayUnicoNumbers =uniq(array.arrayNumeros);
+const arrayUnicoNomes =uniq(array.arrayNomes);
+console.log(arrayUnicoNumbers)
+console.log(arrayUnicoNomes)
+
 
